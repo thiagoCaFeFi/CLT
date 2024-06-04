40d3a9975d26298c32b7264cb7fec85d87388159
thiagoCaFeFi <141078848+thiagoCaFeFi@users.noreply.github.com>
2024-06-03 23:49:26 -0300

    Add files via upload
    Arquivos 2.0

---
+++ MeuJogoJava/src/Main.java
@@ -0,0 +1,21 @@
+package MeuJogoJava.src;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Combate teste = new Combate();
+        Scanner leitor = new Scanner(System.in);
+        int continua = 1;
+        while( continua == 1) {
+            teste.batalha();
+            System.out.println("Fim de Jogo!");
+            continua =2;
+            leitor.close();
+            
+        }
+        
+    }
+}
+
