369b82a4f399338e337bd5c1cd780ab92c6803e9
thiagoCaFeFi <141078848+thiagoCaFeFi@users.noreply.github.com>
2024-06-03 23:54:00 -0300

    Add files via upload

---
+++ MeuJogoJava/src/Arqueiro.java
@@ -0,0 +1,14 @@
+package MeuJogoJava.src;
+
+public class Arqueiro extends Personagem {
+
+    public Arqueiro(String nome, int vida, int mana, int nivel) {
+        super(nome, vida, mana, nivel);
+        setVida(getVida() + getConstitution());
+    }
+
+    @Override
+    public String getClasse() {
+        return "Arqueiro";
+    }
+}
