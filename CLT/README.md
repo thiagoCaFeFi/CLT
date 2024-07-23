2655335dd657074cfdbc43c461fc1b01fd630a5b
thiagoCaFeFi <thiaguinho.tulio@gmail.com>
2024-07-23 00:12:38 -0300

    CheckPoint4
    CheckPoint4

--- CLT/README.md
+++
@@ -1,2 +0,0 @@
-# CLT
-A turn-based game with map exploration and immersive history called "Crônicas de Luz e Trevas".
--- JogoJava/MeuJogoJava/src/Main.java
+++
@@ -1,9 +0,0 @@
-package MeuJogoJava.src;
-//import MeuJogoJava.lib.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
